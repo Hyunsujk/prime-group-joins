d9b13c05af9635a723950c80172b42c1b05bc3d2
Hyunsu Kim <hyunsu.j.kim@gmail.com>
2020-04-21 14:05:45 -0500

    worked on stertch goal

--- joins-solution.sql
+++ joins-solution.sql
@@ -40,3 +40,19 @@ SELECT SUM("on_hand") as "total_num_of_diet_pepsi" FROM "warehouse_product"
 JOIN "warehouse" ON "warehouse_product".warehouse_id = "warehouse".id
 JOIN "products" ON "warehouse_product".product_id = "products".id
 WHERE "products".description = 'diet pepsi';
+
+-- 9. How much was the total cost for each order?
+SELECT "customers".first_name, "customers".last_name, SUM("unit_price"*"quantity") as "total_cost"  FROM "customers"
+JOIN "addresses" ON "customers".id = "addresses".customer_id
+JOIN "orders" ON "addresses".id = "orders".address_id
+JOIN "line_items" ON "orders".id = "line_items".order_id
+JOIN "products" ON "line_items".product_id = "products".id
+GROUP BY "orders".id, "customers".first_name, "customers".last_name;
+
+-- 10. How much has each customer spent in total?
+SELECT SUM("unit_price"*"quantity") as "total_cost", "customers".first_name, "customers".last_name FROM "customers"
+JOIN "addresses" ON "customers".id = "addresses".customer_id
+JOIN "orders" ON "addresses".id = "orders".address_id
+JOIN "line_items" ON "orders".id = "line_items".order_id
+JOIN "products" ON "line_items".product_id = "products".id
+GROUP BY "customers".first_name, "customers".last_name;
